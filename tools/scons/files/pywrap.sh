4bef7e503f70cae6ef2d9d60f9a47012f0715a43
Orual <orual.nbc@proton.me>
2025-02-25 16:12:49 -0500

    fix pywrap script to not exclude python 3

--- tools/scons/files/pywrap.sh
+++ tools/scons/files/pywrap.sh
@@ -5,11 +5,14 @@ case "${0##*/}" in
 	*) arg1="$0.py" ;;
 esac
 
-for bin in python python2 python2.7 python2.6 python2.5 python2.4; do
+for bin in python python3 python2 python3.7 python3.6 python3.5 python2.7 python2.6 python2.5; do
     case "$($bin -V 2>&1)" in
         "Python 2"*) exec $bin $arg1 "$@" ;;
     esac
+    case "$($bin -V 2>&1)" in
+        "Python 3"*) exec $bin $arg1 "$@" ;;
+    esac
 done
 
-echo "Unable to find a Python 2.x interpreter for executing ${arg1:+$arg1 }$@ !" >&2
+echo "Unable to find a Python interpreter for executing ${arg1:+$arg1 }$@ !" >&2
 exit 1
